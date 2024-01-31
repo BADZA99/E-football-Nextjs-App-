a4bec85cd1e328e112309f6d3066eb2e625fe14e
papabn <pndiaye999@gmail.com>
2024-01-31 03:30:19 +0000

    Add new images and update styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex ">
+    <main className="flex text-textPrimary">
       hello world 
     </main>
   );
