5dd782bb9ccfaef63e309f18d8003c5b7d380ea8
papabn <pndiaye999@gmail.com>
2024-02-03 09:56:17 +0000

    Fix optional chaining in league pages

--- src/app/(Leagues)/bundesliga/page.tsx
+++ src/app/(Leagues)/bundesliga/page.tsx
@@ -4,9 +4,10 @@ import LeagueTable from "@/app/components/LeagueTable";
 
 const Bundesliga = async () => {
   const getBundesliga = await filterLeague('Bundesliga')
+
   return (
     <div className='w-[600px]'>
-      {getBundesliga.map((data) => (
+      {getBundesliga?.map((data) => (
         <div key={data.id}>
           <LeagueTable data={data} />
         </div>
